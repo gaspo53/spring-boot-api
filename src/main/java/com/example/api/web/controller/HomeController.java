273a7dafd7ae3028043ce66e6c40af4c5b71611d
Gaspar Rajoy <gaspo53@gmail.com>
2017-03-07 00:23:51 -0300

    fixes

--- src/main/java/com/example/api/web/controller/HomeController.java
+++ src/main/java/com/example/api/web/controller/HomeController.java
@@ -7,7 +7,7 @@ import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * Home controller
- * 
+ *
  * @author Gaspar Rajoy <a href="mailto:gaspo53@gmail.com">gaspo53@gmail.com</a>
  *
  */
@@ -15,14 +15,14 @@ import springfox.documentation.annotations.ApiIgnore;
 @RestController
 public class HomeController extends CommonController{
 
-	
-	
+
+
 	@RequestMapping(value={"","/"})
 	public String helloWord(){
-		return "Hello world from Example API";
+    return "Hello world from "+this.getEnv().getProperty("application.name");
 	}
-	
 
-	
-	
+
+
+
 }
